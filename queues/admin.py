5bcbfaa414895a3c1c1a1fc1317aba910e39e93c
regoeee0055 <regoeee0055@gmail.com>
2025-12-17 00:20:35 +0700

    Delete queues directory

--- queues/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Visit, VitalSign, Queue, TriageResult
-
-admin.site.register(Visit)
-admin.site.register(VitalSign)
-admin.site.register(Queue)
-admin.site.register(TriageResult)
--- queues/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import VitalSign
-
-class VitalSignForm(forms.ModelForm):
-    class Meta:
-        model = VitalSign
-        fields = ["rr", "pr", "sys_bp", "dia_bp", "bt", "o2sat"]
