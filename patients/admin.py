6c37cfbe40d86d4e1daf47515490e8cc4cc523aa
regoeee0055 <regoeee0055@gmail.com>
2025-12-17 00:20:41 +0700

    Delete patients directory

--- patients/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Patient
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ("cid", "hn", "first_name", "last_name", "phone", "created_at")
-    search_fields = ("cid", "hn", "first_name", "last_name")
--- patients/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("register/", views.register_patient, name="register_patient"),
-]
